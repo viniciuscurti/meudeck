da9911034f3dc6963bfd419bf7dbd4f3156468c2
gabriel belo <gabrielrbelo@gmail.com>
2020-08-25 17:53:29 -0300

    Card controller ready

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,6 @@
 class CardsController < ApplicationController
   before_action :set_card, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @cards = Card.all
@@ -14,23 +15,29 @@ class CardsController < ApplicationController
 
   def create
     @card = Card.new(card_params)
-    @card.save
-    #caminho
+    @card.user = current_user
+    if @card.save
+      redirect_to card_path(@card), notice: "card was successfully created"
+    else
+      render :new
+    end
   end
-  
+
   def edit
   end
 
   def update
-    @card.update(card_params)
-    #caminho
+    if @card.update(card_params)
+      redirect_to card_path(@card), notice: "card was successfully updated"
+    else
+      render :edit
+    end
   end
 
   def destroy
     @card.destroy
-    #caminho
+    redirect_to cards_path, notice: "card was successfully deleted"
   end
-
 end
 
 private
@@ -40,5 +47,5 @@ def set_card
 end
 
 def card_params
-  params.require(:card).permit()
+  params.require(:card).permit(:category, :name, :description, :price)
 end
