28a1da616aa18bd37242e9b66207b81be15ce75c
gabriel belo <gabrielrbelo@gmail.com>
2020-08-27 20:37:37 -0300

    Index card done; Card and Order views changed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @last_cards = Card.order(:created_at).last(5)
+  end
+
+  def my_profile
+
   end
 end
